a71b72eedcf90c37cd935eebef143e41eada5766
imzoughene <amzian.elyass@gmail.com>
2018-09-14 23:36:05 +0100

    If Elif Else

---
+++ Condition_If_Else.py
@@ -0,0 +1,8 @@
+Number = input("Enter a number")
+if(int(Number)>0):
+    print("You entred a positive number")
+elif(int(Number)==0):
+    print("zero has no sign")
+else:
+    print("You entred a negative number")
+print("App is Done")
---
+++ elif.py
@@ -0,0 +1,12 @@
+Degree = int(input("Enter you Degree : "))
+if(Degree>90):
+    print("You're score is A")
+elif(Degree>80 and Degree<=90):
+    print("You're score is B")
+elif(Degree>70 and Degree<=80):
+    print("You're score is C")
+elif(Degree>60 and Degree<=70):
+    print("You're score is D")
+else:
+    print("You're fail")
+print("App is Done")
