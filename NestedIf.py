4c30c4c9f8442428a9509ea41a4f25c7d3a5ef75
imzoughene <amzian.elyass@gmail.com>
2018-09-16 19:43:24 +0100

    Nested If,While Loop

---
+++ NestedIf.py
@@ -0,0 +1,9 @@
+Age = int(input("Enter your Age : "))
+if(Age>=18):
+    print("Welcome you are elegible")
+    if(Age<30):
+        print("Your class is A")
+    else:
+        print("your class is B")
+else:
+    print("Sorry we can not enroll you in the study")
---
+++ WhileLoop.py
@@ -0,0 +1,6 @@
+i = 0
+while(i<23):
+    if(i%2==1):
+        print("hello youssef imzoughene {}".format(i))
+    i=i+1
+print("Program Done")
