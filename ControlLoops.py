4becc316ad410b3dc8e5f78cf55281fb38d74fbc
imzoughene <amzian.elyass@gmail.com>
2018-09-17 13:06:07 +0100

    Quize,main function

---
+++ ControlLoops.py
@@ -0,0 +1,12 @@
+word = "imzoughene"
+for letter in word:
+
+    if(letter=="o"):
+        continue
+    print(letter)
+
+    if(letter=="g"):
+        break
+
+
+print("App is Done")
---
+++ MainFile.py
@@ -0,0 +1,6 @@
+def main():
+    # Main code function
+    print("hello imzoughene")
+
+
+if __name__ == '__main__':main()
