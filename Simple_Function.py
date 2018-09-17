4e7ce4b4f1591a826e6188f2c0681fc6006ee4f4
imzoughene <amzian.elyass@gmail.com>
2018-09-17 14:27:12 +0100

    Simple Function

---
+++ Simple_Function.py
@@ -0,0 +1,14 @@
+def Display():
+    print("hello imzoughene")
+    print("Welcome to Python")
+def sum(n1,n2):
+    z=n1+n2
+    return z
+def main():
+    Display()
+    print("Sum : {}".format(sum(5,7)))
+    print("Sum : {}".format(sum(87,54)))
+    print("Sum : {}".format(sum(34,65)))
+
+
+if __name__ == '__main__':main()
