1bf4b66e08749155f97c96c74ec59cb99b4cb02b
imzoughene <amzian.elyass@gmail.com>
2018-10-01 19:13:07 +0100

    inheritance

---
+++ Class_Operations.py
@@ -0,0 +1,18 @@
+class Operations:
+    def sum(self,n1,n2):
+        return n1+n2
+    def div(self,n1,n2):
+        return n1/n2
+
+class ChildOperations(Operations):
+    def multi(self,n1,n2):
+        return n1*n2
+
+def main():
+    myTEST= ChildOperations()
+    print("multi : {}".format(myTEST.multi(4,7)))
+    print("multi : {}".format(myTEST.sum(4,7)))
+    print("multi : {}".format(myTEST.div(4,7)))
+
+
+if __name__ == '__main__':main()
