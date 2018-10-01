84bb6b6c916d4695b14479389be8a0f9ebac4b07
imzoughene <amzian.elyass@gmail.com>
2018-10-01 19:21:35 +0100

    OverRide

--- Class_Operations.py
+++ Class_Operations.py
@@ -7,6 +7,9 @@ class Operations:
 class ChildOperations(Operations):
     def multi(self,n1,n2):
         return n1*n2
+    def sum(self,n1,n2):
+        #return (n1+n2)*2
+        return super().sum(n1,n2)*4
 
 def main():
     myTEST= ChildOperations()
