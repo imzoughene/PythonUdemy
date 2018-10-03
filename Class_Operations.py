1861ec78cc383e075e5b93dc101361d1db79683f
imzoughene <amzian.elyass@gmail.com>
2018-10-03 16:49:41 +0100

    create new Model

--- Class_Operations.py
+++ Class_Operations.py
@@ -10,12 +10,3 @@ class ChildOperations(Operations):
     def sum(self,n1,n2):
         #return (n1+n2)*2
         return super().sum(n1,n2)*4
-
-def main():
-    myTEST= ChildOperations()
-    print("multi : {}".format(myTEST.multi(4,7)))
-    print("multi : {}".format(myTEST.sum(4,7)))
-    print("multi : {}".format(myTEST.div(4,7)))
-
-
-if __name__ == '__main__':main()
---
+++ StartRun.py
@@ -0,0 +1,8 @@
+import Class_Operations
+def main():
+    myTEST= Class_Operations.ChildOperations()
+    print("multi : {}".format(myTEST.multi(4,7)))
+    print("multi : {}".format(myTEST.sum(4,7)))
+    print("multi : {}".format(myTEST.div(4,7)))
+
+if __name__ == '__main__':main()
