0bc9ebc3e7a72e7e39ff38e644b0ade669febb89
Youssef Imzoughene <amzianx@gmail.com>
2019-08-10 15:14:22 +0100

    buttons

--- python GUI/main.py
+++ python GUI/main.py
@@ -3,4 +3,12 @@ import ttk
 root = Tk()
 button = ttk.Button(root,text='imzoughene')
 button.pack()
+def buClick():
+    print('imzoughene is a great person')
+    
+button.config(command=buClick)
+#button.invoke()
+#button.state(['disabled']) # disable the button
+#button.state(['!disabled']) # enable the button
+#button.instate(['disabled']) # check if button is disabled = false
 root.mainloop()
